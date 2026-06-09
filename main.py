4041e18a0834302d26df0b35c8d86b06db03a584
bangdakimon <christykewas@gmail.com>
2026-06-09 15:15:54 +0700

    manim anu
    ngetes ngetes dikit

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from manim import *
 
-
-class SquareToCircle(Scene):
+class TestManim(Scene):
     def construct(self):
-        square = Square()
-        circle = Circle()
-        circle.set_fill(opacity=0.5)
-
-        self.play(Create(square))
-        self.play(Transform(square, circle))
-        self.play(FadeOut(circle))
-
-class HelloMath(Scene):
-    def construct(self):
-        text = Text("Gabut tapi produktif")
-        formula = MathTex(r"E = mc^2").next_to(text, DOWN)
-
-        self.play(Write(text))
-        self.play(Write(formula))
+        title = Text("Manim works on Windows!")
+        self.play(Write(title))
         self.wait(1)
